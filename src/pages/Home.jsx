f27015b0ddd1d7c0887d81a36ce2c37fb241f2c0
shafiquddin <shafiquddin2k@gmail.com>
2024-06-20 17:04:24 +0530

    Navigate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
+    const navigateHandler = () => {
+        navigate('/products')
+    }
     return <>
     <h1>My Home page</h1>
+    <p>
+        <button onClick={navigateHandler}>Products</button>
+    </p>
     </>
 }
 
